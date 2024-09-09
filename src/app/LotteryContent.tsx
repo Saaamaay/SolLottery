c59d7c8f304bee3f333aed2a8fccb57bdd6f9cd2
Saaamaay <sam.masker@entityx.ai>
2024-09-09 15:37:25 +0100

    nomfix2

--- src/app/LotteryContent.tsx
+++ src/app/LotteryContent.tsx
@@ -68,7 +68,7 @@ const LotteryContent: React.FC = () => {
 
   return (
     <div className="max-w-4xl mx-auto">
-      <h1 className="text-4xl font-bold mb-4">Sepolia Lottery</h1>
+      <h1 className="text-4xl font-bold mb-4">Sams Sepolia Lottery</h1>
       <p className="text-xl mb-6">Deposit to win!</p>
       
       <div className="flex gap-4 mb-8">
